7795dcc5a8c2197cf6efac654c0c1a7c0484b6d8
Vova <vova@example.com>
2026-03-11 08:42:38 +0200

    Fix: Cannot GET /menu and design restore

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function addToCart(name, price) {
-    // Отримуємо список або створюємо порожній масив
     let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-    
-    // Додаємо об'єкт товару
+
     cart.push({ name: name, price: Number(price) });
     
-    // Зберігаємо
     sessionStorage.setItem('cart', JSON.stringify(cart));
     
     alert("Додано: " + name);
